156f26ab6973948284e7335841dff3dc84733f08
Ayaan Adil <ayaanadil@Ayaans-MacBook-Air.local>
2022-11-07 15:41:10 +1100

    changes

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,7 @@ class LoginForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField("Log In")
 
 class admin_LoginForm(FlaskForm):
     id = StringField('ID', validators=[DataRequired()])
